40487f3e7f2ada98b14465ffd59db1679cf67615
vabs10 <vyagneshwar98@gmail.com>
2024-09-16 23:23:35 +0530

    Added img

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "src/BasicNft.sol";
 
-contract DeployBasicNft is Script{
-    function run() external returns(BasicNft){
+contract DeployBasicNft is Script {
+    function run() external returns (BasicNft) {
         vm.startBroadcast();
         BasicNft basicNft = new BasicNft();
         vm.stopBroadcast();
--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -6,30 +6,31 @@ import {Test} from "forge-std/Test.sol";
 import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
 import {BasicNft} from "src/BasicNft.sol";
 
-contract BasicNftTest is Test{
+contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
     address public USER = makeAddr("user");
-    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    function setUp() public{
+    function setUp() public {
         deployer = new DeployBasicNft();
         basicNft = deployer.run();
     }
 
-    function testNameIsCorrect() public view{
+    function testNameIsCorrect() public view {
         string memory expectedName = "Billu";
         string memory actualName = basicNft.name();
         assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
     }
 
-    function testSymbolIsCorrect() public view{
+    function testSymbolIsCorrect() public view {
         string memory expectedSymbol = "CAT";
         string memory actualSymbol = basicNft.symbol();
         assert(keccak256(abi.encodePacked(expectedSymbol)) == keccak256(abi.encodePacked(actualSymbol)));
     }
 
-    function testCanMintAndHaveBalance() public{
+    function testCanMintAndHaveBalance() public {
         vm.prank(USER);
         basicNft.mintNft(PUG);
 
